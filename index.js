3c831824e14f242df84d4ea7cf1c586a15a073d6
brenomarq <brenomarques516@gmail.com>
2023-12-19 09:42:51 -0300

    Added some style

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
