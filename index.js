ae209f17f1f45b004b9ca6b7d3cc5477eeaae397
brenomarq <brenomarques516@gmail.com>
2023-12-19 18:06:57 -0300

    Implementing posts route and logic

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const userPosts = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Pages routes
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.render("home.ejs", {listedPosts: userPosts});
 });
 
 app.get("/about", (req, res) => {
@@ -20,7 +21,12 @@ app.get("/contact", (req, res) => {
     res.render("contact.ejs");
 });
 
-//Methods associated with posts
+// Methods associated with posts
+app.post("/create", (req, res) => {
+    const createdPost = req.body;
+    userPosts.push(createdPost);
+    res.redirect("/");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
